48849c6de7774be1745d09b651f52bd73446bdc3
Douglas Miner <curr3nas@gmail.com>
2020-06-09 04:40:11 -0500

    added form validation, and error handling components to AddFolder and AddNote

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -3,8 +3,17 @@ import NotefulForm from '../NotefulForm/NotefulForm'
 import ApiContext from '../ApiContext'
 import config from '../config'
 import './AddFolder.css'
+import ErrorPage from '../Errors/ErrorPage'
+import ValidationError from '../Errors/ValidationError'
 
 export default class AddFolder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: { value: '', touched: false },
+
+    }
+  }
   static defaultProps = {
     history: {
       push: () => { }
@@ -38,23 +47,46 @@ export default class AddFolder extends Component {
       })
   }
 
+  updateName(name) {
+    this.setState({name: {value: name, touched: true}})
+  }
+
+  validateName() {
+    const name = this.state.name.value.trim();
+    if(name.length === 0) {
+      return 'Name is required';
+    } else if(name.length < 3) {
+      return 'Name should be at least 3 characters long'
+    }
+  }
+
   render() {
     return (
       <section className='AddFolder'>
         <h2>Create a folder</h2>
-        <NotefulForm onSubmit={this.handleSubmit}>
+        <ErrorPage>
+          <NotefulForm onSubmit={this.handleSubmit}>
           <div className='field'>
             <label htmlFor='folder-name-input'>
               Name
             </label>
-            <input type='text' id='folder-name-input' name='folder-name' />
+            <input type='text' id='folder-name-input' name='folder-name' onChange={e => this.updateName(e.target.value)}/>
+            {this.state.name.touched && <ValidationError message={this.validateName()} /> }
           </div>
           <div className='buttons'>
-            <button type='submit'>
+            <button 
+            type='submit'
+            className="folder-submit"
+            disabled={
+              this.validateName()
+            }
+            >
               Add folder
             </button>
           </div>
         </NotefulForm>
+        </ErrorPage>
+        
       </section>
     )
   }
--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -3,8 +3,29 @@ import NotefulForm from '../NotefulForm/NotefulForm'
 import ApiContext from '../ApiContext'
 import config from '../config'
 import './AddNote.css'
+import ErrorPage from '../Errors/ErrorPage'
+import ValidationError from '../Errors/ValidationError'
 
 export default class AddNote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: {
+        value: '',
+        touched: false
+      },
+      content: {
+        value: '',
+        touched: false
+      },
+      folderId: {
+        value: '',
+        thouched: false
+      }
+    }
+  }
+
+
   static defaultProps = {
     history: {
       push: () => { }
@@ -14,10 +35,13 @@ export default class AddNote extends Component {
 
   handleSubmit = e => {
     e.preventDefault()
+    
+    const { name, content, folderId } = this.state
+    
     const newNote = {
-      name: e.target['note-name'].value,
-      content: e.target['note-content'].value,
-      folderId: e.target['note-folder-id'].value,
+      name: name.value,
+      content: content.value,
+      folderId: folderId.value,
       modified: new Date(),
     }
     fetch(`${config.API_ENDPOINT}/notes`, {
@@ -41,29 +65,65 @@ export default class AddNote extends Component {
       })
   }
 
+  updateNoteName(name) {
+    this.setState({name: {value: name, touched: true}})
+  }
+
+  updateContent(content) {
+    this.setState({content: {value: content, touched: true}})
+  }
+
+  updateFolderId(folderId) {
+    this.setState({folderId: {value: folderId, touched: true}})
+  }
+
+  validateName() {
+    const name = this.state.name.value.trim();
+    if (name.length === 0) {
+      return 'Name is required';
+    }
+  }
+
+  validateContent() {
+    const content = this.state.content.value.trim();
+    if (content.length === 0) {
+      return 'Content is required';
+    }
+  }
+
+  validateFolderId() {
+    const folderId = this.state.folderId.value;
+    if (folderId === '' || folderId === '...' || !folderId) {
+      return 'Please select a folder';
+    }
+  }
+
   render() {
     const { folders=[] } = this.context
     return (
       <section className='AddNote'>
         <h2>Create a note</h2>
-        <NotefulForm onSubmit={this.handleSubmit}>
+        <ErrorPage>
+          <NotefulForm onSubmit={this.handleSubmit}>
           <div className='field'>
             <label htmlFor='note-name-input'>
               Name
             </label>
-            <input type='text' id='note-name-input' name='note-name' />
+            <input type='text' id='note-name-input' name='note-name' onChange={e => this.updateNoteName(e.target.value)}/>
+            {this.state.name.touched && <ValidationError message={this.validateName()} />}
           </div>
           <div className='field'>
             <label htmlFor='note-content-input'>
               Content
             </label>
-            <textarea id='note-content-input' name='note-content' />
+            <textarea id='note-content-input' name='note-content' onChange={e => this.updateContent(e.target.value)}/>
+            {this.state.content.touched && <ValidationError message={this.validateContent()} />}
           </div>
           <div className='field'>
             <label htmlFor='note-folder-select'>
               Folder
             </label>
-            <select id='note-folder-select' name='note-folder-id'>
+            <select id='note-folder-select' name='note-folder-id' onChange={e => this.updateFolderId(e.target.value)}>
               <option value={null}>...</option>
               {folders.map(folder =>
                 <option key={folder.id} value={folder.id}>
@@ -71,13 +131,23 @@ export default class AddNote extends Component {
                 </option>
               )}
             </select>
+            {this.state.folderId.touched && <ValidationError message={this.validateFolderId()} />}
           </div>
           <div className='buttons'>
-            <button type='submit'>
+            <button 
+            type='submit'
+            className='note-submit'
+            disabled={
+              this.validateName() ||
+              this.validateFolderId() ||
+              this.validateContent()
+            }
+            >
               Add note
             </button>
           </div>
         </NotefulForm>
+        </ErrorPage>
       </section>
     )
   }
